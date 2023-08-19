4953e739c31994f96f191e0a9df5442ce06ba9b1
AnandKumar <anandkumar031199@gmail.com>
2023-08-19 16:17:51 +0530

    commit it

--- src/Pages/Songs/All/All.jsx
+++ src/Pages/Songs/All/All.jsx
@@ -1,11 +1,27 @@
 import React, { useContext } from "react";
 import Track from "../../../Components/Track/Track";
+// import {audio}  from '../../'
 import { MyContext } from "../../../MyContext";
 import "./All.css";
+import { audio } from "../../../Components/Audios";
 const url = "https://academics.newtonschool.co/api/v1/music/song";
 const All = () => {
   const { songPlay } = useContext(MyContext);
-
+  if (songPlay.length == 0) {
+    return (
+      <>
+        <div className="sound_cloud-songs_all">
+          <div className="sound_cloud-songs_all_list">
+            {audio.slice(100, 120)?.map((item, i, self) => (
+              <div key={item?._id} className="sound_cloud-songs_all_list_item">
+                <Track key={item?._id} song={item} index={i} self={self} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <div className="sound_cloud-songs_all">
